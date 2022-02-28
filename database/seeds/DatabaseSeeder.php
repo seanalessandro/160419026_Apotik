71caaf4ad1e368e778ded72f9e3574289e933e8e
seanalessandro <seanalessandrolianto@gmail.com>
2022-02-28 23:15:30 +0700

    Tambah UserSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(MedicineSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
